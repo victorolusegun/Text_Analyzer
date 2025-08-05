5889f1804c715803a90e86d8aafa3e631b7c8a96
Victor Olusegun <victorolusegun003@gmail.com>
2025-08-05 10:58:59 +0100

    fixed error handling for file path input

--- text_analyzer.py
+++ text_analyzer.py
@@ -1,11 +1,16 @@
-#UserName
+# Importing Library
+import re
+
+# UserName
 name = input('What is your name?\n')
 print(f'Welcome {name}!!!')
-#Input Options
+
+# Input Options
 src_opt = ['Enter manually', 'Read from file']
 for index, item in enumerate(src_opt):
     print(f'{index + 1}. {item}')
 
+# Validating User Choice
 try:
     opt = input('How would you like to provide text\n')
     opt = int(opt)
@@ -15,16 +20,23 @@ except ValueError:
         opt = input('Please input numerical values\n')
     opt = int(opt)
 
+# Accepting Text To Be Analyzed and Cleaned
 if opt == 2:
-    try:
-        text = input('Please enter file path\n')
-    except FileNotFoundError:
-        print('Invalid file path!!!')
-        text = input('Please enter valid file path')
-    with open(text, 'r') as t_file:
-        file_contents = t_file.read()
-        print(file_contents)
+    while True:
+        try:
+            text = input('Please enter file path\n')
+            with open(text, 'r') as t_file:
+                file_contents = t_file.read()
+            break
+        except FileNotFoundError:
+            print('Invalid file path!!!')
+        except Exception:
+            print('An unexpected error occured!!!Please try again')
+    # Cleaning Text
+    clean_text = re.sub(r'/s+', '', file_contents).strip()
 elif opt == 1:
     text = input('Please input text\n')
+    #Cleaning Text
+    clean_text = re.sub(r'/s+', '', text).strip()
 else:
     print('Only two options are currently available')
